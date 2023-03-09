c655a9e03645601291d1b0566aba7f2fa85b1c57
Mouiad <66889657+Mouiad-JRA@users.noreply.github.com>
2023-03-09 08:05:09 +0300

    Add unit test #8 (#10)
    * feat: Use DRF #5
    - add home view and home.html
    - add upload_game view and upload_game.html
    
    * feat: Use DRF #5
    - add ranking view and ranking_table.html
    - add GameListCreateAPIView
    - add GameRetrieveUpdateDestroyAPIView
    
    * feat: Add unit test #8
    
    ---------
    
    Co-authored-by: Mouiad Ali <mouiad@kuwaitnet.dev>

---
+++ sport_league/core/tests/test_serializers.py
@@ -0,0 +1,29 @@
+from django.test import TestCase
+
+from core.models import Team
+from core.serializers import TeamSerializer
+
+
+class TeamSerializerTestCase(TestCase):
+
+    def setUp(self):
+        self.team_data = {'name': 'Manchester United'}
+        self.team = Team.objects.create(name='Chelsea')
+
+    def tearDown(self):
+        self.team.delete()
+
+    def test_valid_data(self):
+        serializer = TeamSerializer(data=self.team_data)
+        self.assertTrue(serializer.is_valid())
+        team = serializer.save()
+        self.assertEqual(team.name, self.team_data['name'])
+
+    def test_blank_name(self):
+        self.team_data['name'] = ''
+        serializer = TeamSerializer(data=self.team_data)
+        self.assertFalse(serializer.is_valid())
+        self.assertEqual(set(serializer.errors.keys()), {'name'})
+        self.assertEqual(
+            str(serializer.errors['name'][0]), 'This field may not be blank.'
+        )
