2a5a95ffb219d7133fba9400793e93d70ab60779
Mouiad Ali <mouiad@kuwaitnet.dev>
2023-03-11 10:35:26 +0300

    fix(Docker)

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
 # Migrate
+cd sport_league
 python manage.py makemigrations
 python manage.py migrate
 
 # Compile messages
-python manage.py compilemessages
+#python manage.py compilemessages
 
 exec "$@"
